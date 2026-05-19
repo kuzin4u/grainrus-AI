972b824ceb21f9bf08f5a25fc721554a0862589e
Sergey Kuzin <33122639+kuzin4u@users.noreply.github.com>
2026-05-19 23:55:14 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const API_KEY = process.env.PROXY_API_KEY || process.env.DEEPSEEK_API_KEY;
 const API_BASE_URL = process.env.API_BASE_URL || 'https://openai.api.proxyapi.ru/v1/chat/completions';
-const MODEL_NAME = process.env.MODEL_NAME || 'deepseek-chat';
+const MODEL_NAME = process.env.MODEL_NAME || 'deepseek/deepseek-chat';
 // const MODEL_NAME='openrouter/openrouter/free';
 if (!API_KEY) console.error('❌ API-ключ не задан!');
 
